1cc6067db01b2647df971300dd7a5919b16e5145
RishiBaskaran <68693669+RishiBaskaran@users.noreply.github.com>
2020-08-25 10:26:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 var bin1,bin2,bin3;
-
+var ground;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var paperball
+var paperball;
 
 
 function setup() {
@@ -22,22 +22,30 @@ function setup() {
 	
 	bin2 = new Bin(1100,610,20,100);
 	bin3 = new Bin(1175,650,168,20);
-	paperball = new Paperball(800,350,20);
+	paperball = new Paperball(300,350,15);
+	ground = new Ground(800,680,1600,20)
 }
 
 
 function draw() {
   rectMode(CENTER);
-  background(0);
+  background(100);
   
   
- 
   drawSprites();
   bin1.display();
   bin2.display();
   bin3.display();
   paperball.display();
+  ground.display();
+  keyPressed();
 }
+function keyPressed(){
+if(keyCode === UP_ARROW){
 
+	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+
+}
+}
 
 
