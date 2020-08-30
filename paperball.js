5fcc2676650d23f7550883f3c92cb7bbf2b46221
RishiBaskaran <68693669+RishiBaskaran@users.noreply.github.com>
2020-08-30 10:21:23 +0530

    Update paperball.js

--- paperball.js
+++ paperball.js
@@ -3,8 +3,8 @@ class Paperball {
       var options = {
           isStatic : false,
           restitution : 0.3,
-          friction : 0.5 ,
-          density : 1.2 ,
+          friction : 1.5 ,
+          density : 3.2 ,
       }
       this.radius = radius;
       this.x = x;
